cd119e8102e3400accc5112062b4899dfc55363a
Tomoki Nishinaka <ariaqua713.f7z@gmail.com>
2015-08-20 16:00:05 +0900

    init project

---
+++ NSNL/NSNL/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  NSNL
+//
+//  Created by Nishinaka Tomoki on 8/20/15.
+//  Copyright (c) 2015 Nishinaka Tomoki. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+          }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    
+    }
+
+
+    
+    
+    
+}
+
