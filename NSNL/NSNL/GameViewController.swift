f6bf147b49c2a316554acff6dbf97e68d4e71069
itsuki <ghost_fifty_itsuki_x@yahoo.co.jp>
2015-08-21 13:30:27 +0900

    マップとキャラの作成

--- NSNL/NSNL/GameViewController.swift
+++ NSNL/NSNL/GameViewController.swift
@@ -1,6 +1,6 @@
 //
 //  GameViewController.swift
-//  NSNL
+//  ee
 //
 //  Created by itsuki on 2015/08/20.
 //  Copyright (c) 2015年 itsuki. All rights reserved.
@@ -26,10 +26,10 @@ extension SKNode {
 }
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
             // Configure the view.
             let skView = self.view as! SKView
@@ -45,11 +45,11 @@ class GameViewController: UIViewController {
             skView.presentScene(scene)
         }
     }
-
+    
     override func shouldAutorotate() -> Bool {
         return true
     }
-
+    
     override func supportedInterfaceOrientations() -> Int {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
             return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
@@ -57,12 +57,12 @@ class GameViewController: UIViewController {
             return Int(UIInterfaceOrientationMask.All.rawValue)
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
